dc3a0d5575548b83988093fa7af97b51d635da39
Adarsh <adarshshanu3@gmail.com>
2024-05-31 20:38:35 +0530

    base url changed to ip address for testing

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "http://52.140.0.220/api",
+    // baseURL: "http://localhost:5000/api",
     withCredentials: true
 })
 
