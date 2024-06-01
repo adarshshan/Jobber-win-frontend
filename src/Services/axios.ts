5dee65e1900c22054162e1987cc8dbeba6e0a98a
Adarsh <adarshshanu3@gmail.com>
2024-06-01 11:39:33 +0530

    someting

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-    baseURL: "https://www.jobberwin.top/api",
+    baseURL: "https://jobberwin.top/api",
     // baseURL: "http://localhost:5000/api",
     withCredentials: true
 })
