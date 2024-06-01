7a3050b64cf8ec57d9b20ddd9a8be4adf480837c
Adarsh <adarshshanu3@gmail.com>
2024-06-01 10:12:56 +0530

    base url changed

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-    baseURL: "http://52.140.0.220/api",
+    baseURL: "https://www.jobberwin.top/api",
     // baseURL: "http://localhost:5000/api",
     withCredentials: true
 })
