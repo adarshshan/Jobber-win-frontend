877f65f38de92a3ad35a666ee3746cae822ae017
Adarsh <adarshshanu3@gmail.com>
2024-06-07 14:37:16 +0530

    env changed

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
-console.log(process.env); console.log('this is the secret uri...');
 
 const Api: AxiosInstance = axios.create({
     baseURL: `${process.env.REACT_APP_B_URI}/api`,
